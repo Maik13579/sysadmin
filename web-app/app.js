0c872f3aa55ebff7af0977cd4b94fe6bcf244fae
AJannik <jannik.schmidberger@gmail.com>
2022-04-28 16:04:04 +0200

    added json db for authorized users

--- web-app/app.js
+++ web-app/app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const bcrypt = require('bcrypt');
 const path = require("path");
 const bodyParser = require('body-parser');
-const users = require('./data').userDB;
+const userDataManager = require('./userDataManager');
 
 const app = express();
 const server = http.createServer(app);
@@ -17,27 +17,18 @@ app.get('/', (req,res) => {
 });
 
 app.post('/login', async (req, res) => {
-  // TODO: remove
-  let paswd = await bcrypt.hash("123", 10);
-  let user = {
-    id: Date.now(),
-    username: "jannik",
-    email: "jannik@123.de",
-    password: paswd
-  };
-  users.push(user);
-        console.log(users);
     try{
-        let foundUser = users.find((data) => req.body.email === data.email);
-        if (foundUser) {
-
+        let users = userDataManager.loadUsers();
+        let foundUser = users[0];
+        const emailMatch = await bcrypt.compare(req.body.email, users[0].email);
+        //.find((data) => bcrypt.compare(req.body.email, data.email));
+        if (emailMatch) {
             let submittedPass = req.body.password;
             let storedPass = foundUser.password;
 
             const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
             if (passwordMatch) {
-                let usrname = foundUser.username;
-                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
             } else {
                 res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
             }
@@ -49,12 +40,12 @@ app.post('/login', async (req, res) => {
 
             res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
         }
-    } catch{
-        res.send("Internal server error");
+    } catch(e) {
+        res.send("Internal server error " + e);
     }
 });
 
 
-server.listen(3000, function(){
+server.listen(3000, function() {
     console.log("server is listening on port: 3000");
 });
