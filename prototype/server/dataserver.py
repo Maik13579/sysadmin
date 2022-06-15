8a766b1da9d810f6ce29a96647c025f80fa53118
Lukas Daibert <lukasdaibert1998@gmail.com>
2022-06-15 23:33:41 +0200

    Add python api using fastAPI and uvicorn

---
+++ prototype/server/dataserver.py
@@ -0,0 +1,29 @@
+import glob
+import json
+import io
+
+from starlette.responses import StreamingResponse
+from fastapi import FastAPI
+
+app = FastAPI()
+directory = "./videos/"
+ext = "*.avi"    
+
+@app.get("/videos")
+def get_videos(filename = None):
+    response = None
+    paths = glob.glob(directory + ext)
+    filenames =  []
+    for path in paths:
+        name = path.replace(directory, "")
+        filenames.append(name)
+    if filename is None:
+        json_string = json.dumps(filenames)
+        response = json_string
+    else:
+        if filename not in filenames:
+            return {"Error, please provide a valid filename"}
+        video = open(directory + filename, mode="rb")
+        response = StreamingResponse(video, media_type="video/x-msvideo")
+
+    return response
