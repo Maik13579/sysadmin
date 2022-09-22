86bcd60b56f6e79a5457d9d91a63e10a699e941b
AJannik <jannik.schmidberger@gmail.com>
2022-09-22 15:24:46 +0200

    added archieve endpoints to data api

--- prototype/server/uvicorn_server.py
+++ prototype/server/uvicorn_server.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
                 host="127.0.0.1",
                 port=8000,
                 reload=True,
-                ssl_keyfile="./key.pem", 
-                ssl_certfile="./cert.pem"
+                ssl_keyfile=r"C:\Users\janni\github\sysadmin\prototype\key.pem",
+                ssl_certfile=r"C:\Users\janni\github\sysadmin\prototype\cert.pem"
                 )
