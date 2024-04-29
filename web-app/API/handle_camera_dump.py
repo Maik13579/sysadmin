89869718b809d49f44d104dc83d8799cb53acfc8
AJannik <jannik.schmidberger@gmail.com>
2024-04-29 14:22:04 +0200

    updating script to use MP4Box for converting to mp4 format

--- web-app/API/handle_camera_dump.py
+++ web-app/API/handle_camera_dump.py
@@ -4,6 +4,7 @@ import io
 import sys
 from cryptography.fernet import Fernet
 import moviepy.editor as moviepy
+from subprocess import call
 
 symmetric_key = "zCm3lRnleW3gwiIJfRJGLPTHCrLN08bnkttZG4Wly6c="
 directory = r"C:\Users\janni\github\sysadmin\videos\camera_dump"
@@ -17,14 +18,17 @@ class DumpHandler:
         for root, dirs, files in os.walk(directory):
             for file in files:
                 newFilename = file[:-4] + ".mp4"
-                #self.convert_to_mp4(file, newFilename)
+                self.convert_to_mp4(file, newFilename)
                 #os.remove(os.path.join(directory, file))
-                self.save_file_encrypted(newFilename)
+                #self.save_file_encrypted(newFilename)
 
     def convert_to_mp4(self, filename, newFilename):
         #TODO: use MP4Box for converting
-        clip = moviepy.VideoFileClip(os.path.join(directory, filename))
-        clip.write_videofile(os.path.join(directory, newFilename))
+        #clip = moviepy.VideoFileClip(os.path.join(directory, filename))
+        #clip.write_videofile(os.path.join(directory, newFilename))
+        command = "MP4Box -add {} {}".format(os.path.join(directory, filename), os.path.join(directory, newFilename))
+        call([command], shell=True)
+        print("vid conv")
 
     def save_file_encrypted(self, filename):
         print("Encrypting " + filename)
